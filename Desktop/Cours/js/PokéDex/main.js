7699506e167daf1538aa67915d336c01ca309b4a
thomas <thomas.baullard@gmail.com>
2024-11-28 15:05:53 +0100

    pokédex affiche tous les pokemons dans le tableau et boutons previous/next sont ok

--- Desktop/Cours/js/PokéDex/main.js
+++ Desktop/Cours/js/PokéDex/main.js
@@ -11,49 +11,102 @@ ajouter bouton favoris sur chacune des fiches --> liste paginer
 afficher un message s'il y a une erreur dans l'api et ou si un pokemon n'existe pas 
 
 */
-let next = document.getElementsByClassName("prevNext");
-let best = document.getElementById("favorite");
+
+
+// let next = document.getElementsByClassName("prevNext");
+// let best = document.getElementById("favorite");
+// let pokemon_list = document.getElementById("pokemonList");
+// let pokemon_result = document.getElementById("pokemonResult")
+// let numbersOfItems = 20;
+// let first = 0;
+
+// function nextPage(){
+//     first += numbersOfItems;
+//     showPokemon();
+// }
+
+// async function showPokemon(){
+//     let colonneContainer = document.createElement("div");
+//     const data = await searchPokemon();
+//     for(let i = first; i < first + numbersOfItems; i++){
+//         next[i].addEventListener("click", nextPage)
+//         let colonneContainer = document.createElement("div");
+//         colonneContainer.innerHTML = 
+//         `<div class= "favorite"></div>
+//         <div class= "id">${data[i].id}</div>
+//         <div class= "image"><img src="${data[i].image}" alt="${data[i].name}"/></div>
+//         <div class= "name">${data[i].name}</div>
+//         <div class= "description">${data[i].apiTypes[0].name}</div>`;
+//         colonneContainer.className = "pokemonRow"
+//         pokemon_list.appendChild(colonneContainer); 
+//     }
+// }
+
+// async function searchPokemon() {
+//     try {
+//         const res = await fetch("./pokemon.json");
+//         const json = await res.json();
+//         return json; // Retourner l'objet JSON directement
+//     } catch (error) {
+//         console.error("Erreur lors du chargement du fichier JSON", error);
+//     }
+// }
+// showPokemon();
+
+
+let nextButton = document.getElementById("nextBtn");
+let previousButton = document.getElementById("previousBtn")
 let pokemon_list = document.getElementById("pokemonList");
-let pokemon_result = document.getElementById("pokemonResult")
+let numbersOfItems = 20;
+let firstDisplay = 0;
+let lastDisplay = firstDisplay + numbersOfItems;
+let pokemons;
 
+nextButton.addEventListener('click', () =>{
+    if (lastDisplay < pokemons.length){
+        firstDisplay += numbersOfItems;
+        lastDisplay += numbersOfItems;
+        showPokemon(); 
+    }else {
+    } 
+});
+previousButton.addEventListener('click', () =>{
+    if (firstDisplay != 0){
+        firstDisplay -= numbersOfItems;
+        lastDisplay -= numbersOfItems;
+        showPokemon();
+    }else {
+    }
+});
 
-async function showPokemon(){
-    let colonneContainer = document.createElement("div");
-    const data = await searchPokemon();
-    for(let i = 0; i < 20; i++){
+function showPokemon(){
+    pokemon_list.innerHTML = '';
+        for (let i = firstDisplay; i < lastDisplay; i++){
         let colonneContainer = document.createElement("div");
         colonneContainer.innerHTML = 
-        `<div class= "favorite"></div>
-        <div class= "id">${data[i].id}</div>
-        <div class= "image"><img src="${data[i].image}" alt="${data[i].name}"/></div>
-        <div class= "name">${data[i].name}</div>
-        <div class= "description">${data[i].apiTypes[0].name}</div>`;
-        colonneContainer.className = "pokemonRow"
+        `<div class="favorite">
+            <img src="" alt="">
+        </div>
+        <div class="id">${pokemons[i].id}</div>
+        <div class="image"><img src="${pokemons[i].image}" alt="${pokemons[i].name}"/></div>
+        <div class="name">${pokemons[i].name}</div>
+        <div class="description">${pokemons[i].apiTypes[0].name}</div>`;
+        colonneContainer.className = "pokemonRow";
         pokemon_list.appendChild(colonneContainer);
-
-        
     }
 }
-
+window.onload = async () => {
+    await searchPokemon();
+    console.log(pokemons);
+    showPokemon();
+  };
+  
 async function searchPokemon() {
     try {
-        const res = await fetch("./pokemon.json");
+        const res = await fetch("https://pokebuildapi.fr/api/v1/pokemon");
         const json = await res.json();
-        return json; // Retourner l'objet JSON directement
+        pokemons = json;
     } catch (error) {
         console.error("Erreur lors du chargement du fichier JSON", error);
     }
 }
-showPokemon();
-
-// async function searchPokemon(e) {
-//     e.preventDefault();
-//     console.log('je suis ici');
-//     try {
-//         const data = await fetch(`https://pokebuildapi.fr/api/v1/pokemon`);
-//         const jsonData = await data.json();
-//         best.innerHTML = `<pre><code>${JSON.stringify(jsonData, null, 4)}</pre></code>`
-//     } catch (error) {
-//         console.log("error", error);
-//     }
-// }
